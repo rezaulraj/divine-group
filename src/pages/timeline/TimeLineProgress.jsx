ded43c7fe92424d3bcee20268270a6ba0286cf38
rezaulraj <rezaul.coderpro@gmail.com>
2025-07-28 20:25:46 +0600

    update time line commit

--- src/pages/timeline/TimeLineProgress.jsx
+++ src/pages/timeline/TimeLineProgress.jsx
@@ -239,7 +239,7 @@ const TimeLineProgress = () => {
 
                   {/* Right Side - Text for even, Image for odd */}
                   <motion.div
-                    initial={{ scale: 0.8, opacity: 0, x: 100 }}
+                    initial={{ scale: 0.8, opacity: 0, x: -100 }}
                     whileInView={{ scale: 1, opacity: 1, x: 0 }}
                     viewport={{ once: false, margin: "100px" }}
                     transition={{ duration: 0.8 }}
